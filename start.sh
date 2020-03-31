ff31ed15b43fed75c3e398d6c1973425c83aa424
rockyxia(夏石龙) <rockyxia@webank.com>
2020-03-31 10:09:57 +0800

    modify start.sh,run jmeter at current dictory

--- start.sh
+++ start.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 set -e
+JMETER_HOME=/data/rocky/apache-jmeter-5.1.1-weid 
+#JMETER_HOME=
+if [ -z "$JMETER_HOME" ];then
+echo "please set JMETER_HOME"
+exit 1
+fi
 if [ ! -f "./dist/apps/weid-jmeter-demo-1.0-SNAPSHOT.jar" ];then
 echo "压测程序不存在"
 exit 1
@@ -9,22 +15,26 @@ cp -r ./dist/lib/* "$JMETER_HOME/dependencies/"
 cp -r ./dist/apps/* "$JMETER_HOME/dependencies/"
 fi
 #delete HTTP/*
-if [ -d "$JMETER_HOME/bin/HTTP/" ];then
+if [ -d "http/" ];then
 echo "http file exist,delete history files"
-rm -rf $JMETER_HOME/bin/HTTP/*
+rm -rf http/*
 else
-mkdir $JMETER_HOME/bin/HTTP
+mkdir /http
 fi
 
-if [ ! -f "$JMETER_HOME/bin/demo.jtl" ];then
+if [ ! -f "demo.jtl" ];then
 echo "demo.jtl not exist"
 else
 echo "demo.jtl exist,del..."
-rm -rf $JMETER_HOME/bin/demo.jtl
+rm -rf demo.jtl
 fi
 
-#start jmeter auto test
-jmeter -n -t $JMETER_HOME/bin/demo.jmx -l $JMETER_HOME/bin/demo.jtl -e -o $JMETER_HOME/bin/HTTP
+#at the jmeter home,then start jmeter auto test
+#cp demo.jmx $JMETER_HOME/bin
+#cd $JMETER_HOME/bin
+jmeter -n -t demo.jmx -l demo.jtl -e -o http
+
+#jmeter -n -t $JMETER_HOME/bin/demo.jmx -l $JMETER_HOME/bin/demo.jtl -e -o $JMETER_HOME/bin/HTTP
 
 
 echo "test running end,result http://localhost:9999/index.html"
